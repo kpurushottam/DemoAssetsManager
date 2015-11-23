3485f096c9ad3339eb6dde7ad9f46fdd1831d23e
unknown <purushottam.kumar@user492.aepl.com>
2015-11-23 17:17:35 +0530

    Added: AssetsManager to read raw image file

--- app/src/main/java/com/krp/android/demoassetsmanager/MainActivity.java
+++ app/src/main/java/com/krp/android/demoassetsmanager/MainActivity.java
@@ -1,37 +1,37 @@
 package com.krp.android.demoassetsmanager;
 
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView ivFlower;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ivFlower = (ImageView) findViewById(R.id.iv_flower);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void addFlower(View v) {
+        AssetManager mn = getAssets();
+        try {
+            ivFlower.setImageBitmap(BitmapFactory.decodeStream(mn.open("images/flower.jpg")));
+        } catch (IOException e) {
+            e.printStackTrace();
+            ivFlower.setBackgroundColor(Color.BLUE);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
